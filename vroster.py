f56985afc24e5ced7729c96e619f50e622a1a414
andre.cohen <andre.cohen@52ac0849-1759-e772-d07f-49ffaeb4aacf>
2010-11-20 02:20:00 +0000

    Entirely rewritten code

--- vroster.py
+++ vroster.py
@@ -1,84 +1,92 @@
-import cv
-import sys
-import numpy
+import pickle
 
-import detector
-import classifier
-import util
-import logic
+from vision.util import CVVideo, Image
+from vision.detector import *
+from vision.tracker import *
+from vision.recognizer.LBPRecognizer import *
+from vision.recognizer.BagRecognizer import *
+from vision.matlab.interface import *
+from vision.ui.CVInterface import *
+from vision.ai.trivial import *
 
-for vidID in range(2, 41):
-
-	# Settings
-	moviePath = '/Users/andre/Desktop/videoroster/%03d.mov'%vidID
-	picturePath = '/Users/andre/Desktop/videoroster/photos2/'
-	cascadePath = 'data/opencv-24x24.xml'
-	cascadeSize = (12,12)
-	#cascadePath = '/opt/local/share/opencv/haarcascades/haarcascade_frontalface_alt_tree.xml'
-	#cascadeSize = (15,15)
-	#cascadePath = '/opt/local/share/opencv/haarcascades/haarcascade_frontalface_default.xml'
-	#cascadeSize = (24,24)
-	useUI = False
-	skipFrames = 1
-	clipSize = (32,32)
-
-	# Setup
-	movie = cv.CaptureFromFile(moviePath)
-	detector = detector.HaarDetector(cascadePath, cascadeSize)
-	classifiers = util.createClassifiers(picturePath, classifier.Template, clipSize)
-	logic = logic.RandomLogic()
-	font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, .4, .4)
-
-	if useUI == True:
-		cv.NamedWindow('VRoster')
-
-	frames = []
-
-	while cv.GrabFrame(movie)==1:
-		frame = cv.QueryFrame(movie)
-		frameGray = cv.CreateImage(cv.GetSize(frame), 8, 1)
-		cv.CvtColor(frame, frameGray, cv.CV_RGB2GRAY)
-		cv.EqualizeHist(frameGray, frameGray)
+# UI settings
+ui = CVWindow('VRoster')
 	
-		# Actual work
-		detectedPos = detector.detect(frameGray)
-		detectedImg = util.extractRegions(frameGray, detectedPos, clipSize)
+# Video settings
+videoID = 2
+videoPath = '/Users/andre/Desktop/videoroster/%03d.mov'%videoID
+
+# Photo settings
+photoPath = '/Users/andre/Desktop/videoroster/photos2/'
+
+# Detector settings
+cascadePath = 'data/opencv-24x24.xml'
+cascadeSize = (12,12)
+
+# Recognition
+boundSize = (24, 24)
+
+# Matlab
+matlab = LocalMatlab(10, '/Applications/MATLAB_R2010b.app/', 'maci64')
+matlab.addpath('matlab/')
+matlab.execExpression('addpath(genpath(\'matlab/yalmip\'))', verbose=True)
+
+# Components
+video = CVVideo.CVFileVideo(videoPath)
+detector = HaarDetector(cascadePath, cascadeSize)
+tracker = TrivialTracker()
+ai = dict()
+
+# Recognizer 
+recognizers = []
+for i in range(0, 8):
+	recognizers.append(LBPRecognizer(matlab))
+recognizers = BagRecognizer(photoPath, recognizers, boundSize)
+
+while True:
+	frame = video.next()
+	if frame==None:
+		break
+	frameGray = Image.toGray(frame)
+	
+	# Get objects
+	observations = detector.detect(frameGray)
+	objects = tracker.update(observations)
+	objectImages = Image.extractSubImages(frameGray, objects, boundSize)
+	
+	# Generate recognition matrix
+	recognized = []
+	for image in objectImages:
+		recognized.append(recognizers.query(image))
+
+	# Attempt to find best matching
+	w = numpy.matrix(recognized)
+	res = matlab.vroster_ipo(w, 2, verbose=True)
+	labels = numpy.matrix(res[0])
+	
+	predicted = []
+	for i in range(0, labels.shape[0]):
+		if i not in ai:
+			#ai[i] = MultinomialAI(range(0,9))
+			ai[i] = TrivialAI()
+		ai[i].update(numpy.argmax(labels[i,:]))
+		predicted.append(ai[i].predict())
+			
 	
 	
-		table = numpy.zeros((len(classifiers), 35))
-		for c in range(0, len(classifiers)):
-			for i in range(0, len(detectedImg)):
-				table[c, i] = classifiers[c].query(detectedImg[i])
+	if ui != None:
+		canvas = CVCanvas(frame)
 		
-		frames.append(frames)
-		print vidID, len(frames)
+		for i in range(0, len(objects)):
+			label = predicted[i][0]
+			conf = predicted[i][1]
+						
+			canvas.drawText('%d ~ %.02f'%(label, conf), (objects[i][0], objects[i][1]-3), (255, 255, 0))
+			canvas.drawRect(objects[i], (255,0,0))
+			
+		
+		cv.ShowImage('VRoster', frame)	
+		ui.update(canvas)
 	
-		#results = logic.assign(classifiers, detectedPos, detectedImg)
-
-		#for result in results:
-		#	if result['score']!=0:
-		#		blue = 255 * (1-result['score'])
-		#		red = 255 * result['score']
-	    #
-		#		tl = (result['rect'][0], result['rect'][1])
-		#		lr = (result['rect'][0]+result['rect'][2], result['rect'][1]+result['rect'][3])
-		#		cv.Rectangle(frame, tl, lr, (red,0,blue))
-		#	
-		#		label = '%02d - %.02f'%(result['label'], result['score']) 
-		#		cv.PutText(frame, label, (tl[0], tl[1]-2), font, (0, 255, 0))
-		#
-		## Display video
-		#if useUI == True:
-		#	cv.ShowImage('VRoster', frame)
-	
-		# Skip ahead a bit
-		currentFrame = cv.GetCaptureProperty(movie, cv.CV_CAP_PROP_POS_FRAMES)
-		cv.SetCaptureProperty(movie, cv.CV_CAP_PROP_POS_FRAMES, currentFrame+skipFrames)
-	
-	
-		# Check to quit
-		#if cv.WaitKey(0) == 'q':
-		#	break;
-
-	plicke.dump(frames, open('tracked-%03d.dat'%vidID, 'wb'))
-	
+	#if cv.WaitKey(1)!=-1:
+	#	break
