8b1b112dd47148e1e2f291237033789b07baaeaf
andre.cohen <andre.cohen@52ac0849-1759-e772-d07f-49ffaeb4aacf>
2011-02-04 22:19:50 +0000

    Fixed tracker

--- vision/tracker/TrivialTracker.py
+++ vision/tracker/TrivialTracker.py
@@ -15,38 +15,55 @@ class TrivialTracker(BaseTracker):
 			self.objects = numpy.matrix(observations)
 		if self.objects == None:
 			return []
-		
-		for d in observations:
-			self.objects = numpy.vstack((self.objects, d))
-		
-		while True:
-			objA = []
-			objB = []
+			
+		for observation in observations:
+			bestDistance = []
+			a = self.__center(numpy.asarray(observation))
 			
 			for i in range(0, self.objects.shape[0]):
-				for j in range(0, self.objects.shape[0]):
-					if i!=j and i not in objA and i not in objB and j not in objA and j not in objB:
-						ic = self.__center(numpy.asarray(self.objects[i,:])[0])
-						jc = self.__center(numpy.asarray(self.objects[j,:])[0])
-						if numpy.linalg.norm(ic-jc)<50:
-							objA.append(i)
-							objB.append(j)
-							
-			if objA == []:
-				break
+				b = self.__center(numpy.asarray(self.objects[i,:])[0])
+				bestDistance.append(numpy.linalg.norm(a-b))
+			
+			x = numpy.argmin(bestDistance)
+			if bestDistance[x]<50:
+				self.objects[x, :] += numpy.matrix(observation)
+				self.objects[x, :] *= .5
 			else:
-				new = []
-				for i in range(0, len(objA)):
-					tmp = (self.objects[objA[i],:] + self.objects[objB[i],:])/2.0
-					new.append(tmp)
-				
-				for i in range(0, self.objects.shape[0]):
-					if i not in objA and i not in objB:
-						new.append(self.objects[i,:])
-						
-				self.objects = numpy.vstack(new)
-				
+				self.objects = numpy.vstack((self.objects, observation))
+		
 		return self.getObjects()
+		# 
+		# for d in observations:
+		# 	self.objects = numpy.vstack((self.objects, d))
+		# 
+		# while True:
+		# 	objA = []
+		# 	objB = []
+		# 	
+		# 	for i in range(0, self.objects.shape[0]):
+		# 		for j in range(0, self.objects.shape[0]):
+		# 			if i!=j and i not in objA and i not in objB and j not in objA and j not in objB:
+		# 				ic = self.__center(numpy.asarray(self.objects[i,:])[0])
+		# 				jc = self.__center(numpy.asarray(self.objects[j,:])[0])
+		# 				if numpy.linalg.norm(ic-jc)<50:
+		# 					objA.append(i)
+		# 					objB.append(j)
+		# 					
+		# 	if objA == []:
+		# 		break
+		# 	else:
+		# 		new = []
+		# 		for i in range(0, len(objA)):
+		# 			tmp = (self.objects[objA[i],:] + self.objects[objB[i],:])/2.0
+		# 			new.append(tmp)
+		# 		
+		# 		for i in range(0, self.objects.shape[0]):
+		# 			if i not in objA and i not in objB:
+		# 				new.append(self.objects[i,:])
+		# 				
+		# 		self.objects = numpy.vstack(new)
+		# 		
+		# return self.getObjects()
 		
 	def getObjects(self):
 		if self.objects==None:
