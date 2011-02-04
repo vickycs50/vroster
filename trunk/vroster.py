78021e158c96025c8d6de644f5e1307f64f6d632
andre.cohen@gmail.com <andre.cohen@gmail.com@52ac0849-1759-e772-d07f-49ffaeb4aacf>
2011-02-04 21:51:39 +0000

    Modified algorithm for different 'weights'

--- trunk/vroster.py
+++ trunk/vroster.py
@@ -17,12 +17,13 @@ from vision.ai.gap import *
 
 class VRoster:
 	
-	def __init__(self, alpha=0, beta=0, priorSize=0, frames=300, skipFrames=5):
+	def __init__(self, alpha=0, beta=0, priorSize=0, frames=300, skipFrames=5, ui=False):
 		self.alpha = alpha 
 		self.beta = beta
 		self.priorSize = priorSize
 		self.frames = frames
 		self.skipFrames = skipFrames
+		self.ui = ui
 		
 	def run(self):
 		prev = []
@@ -49,7 +50,7 @@ class VRoster:
 		track = []
 
 		for fid in range(0, self.frames):
-			print fid, '%.02f%%'%(fid/(self.frames*1.0))
+			#print fid, '%.02f%%'%(fid/(self.frames*1.0))
 	
 			for i in range(0, max(self.skipFrames, 1)):
 				frame = video.next()
@@ -74,47 +75,87 @@ class VRoster:
 			# Attempt to find best matching
 			w = numpy.matrix(recognized)
 	
-			prior = numpy.zeros((w.shape[0], w.shape[0]))
+			prior = numpy.zeros(w.shape)
 			priorHistory = numpy.zeros(w.shape)
 	
 			# Calculate total weights 
 			if len(prev)>0 and self.priorSize>0:
+				dist = recognizers.distance()
+				
 				for p in range(0, min(len(prev), self.priorSize)):
 					pid = -1*p
-					for x in itertools.product(range(0, w.shape[0]), repeat=2):
-						if x[0]<len(prev[pid][1]) and x[1]<len(prev[pid][1]):
-							prior[x[0], x[1]] += numpy.linalg.norm(objectImages[x[0]]-prev[pid][1][x[1]])
-					for x in range(0, priorHistory.shape[0]):
-						for y in range(0, priorHistory.shape[1]):
-							if y>=len(prev[pid][0]) or (x<len(prev[pid][0]) and prev[pid][0][x] != y):
-								priorHistory[x, y] += 1
-				for x in range(priorHistory.shape[0]):
-					priorHistory[x,:] /= numpy.sum(priorHistory[x,:])
-					prior[x, :] /= min(len(prev), self.priorSize)*1.0
-	
-				for i in range(0, w.shape[0]):
-					for j in range(0, w.shape[1]):
-						if j<prior.shape[1]:
-							w[i, j] += prior[i,j]*priorHistory[i,j]*self.alpha
+					
+					#print prev[pid][0]
+					
+					for o in range(0, w.shape[0]):
+						if o < len(prev[pid][0]):
+							for r in range(0, w.shape[1]):
+								if prev[pid][0][o] != r:
+									priorHistory[o, r] += 1
+							prior[o, :] += dist[prev[pid][0][o], :]
+				
+				#w2 = w.copy()
+				for o in range(0, w.shape[0]):
+					w[o, :] += priorHistory[o, :]/max(numpy.sum(priorHistory[o, :]),1.0)*prior[o,:]*self.alpha
+				#print numpy.cast[int](w-w2)
+				#print priorHistory/self.priorSize
+				
+						
+			#if len(prev)>0 and self.priorSize>0:
+			#	for p in range(0, min(len(prev), self.priorSize)):
+			#		pid = -1*p
+			#		#for x in itertools.product(range(0, w.shape[0]), repeat=2):
+			#		#	if x[0]<len(prev[pid][1]) and x[1]<len(prev[pid][1]):
+			#		#		prior[x[0], x[1]] += numpy.linalg.norm(objectImages[x[0]]-prev[pid][1][x[1]])
+			#		for x in range(0, priorHistory.shape[0]):
+			#			for y in range(0, priorHistory.shape[1]):
+			#				if y>=len(prev[pid][0]) or (x<len(prev[pid][0]) and prev[pid][0][x] != y):
+			#					priorHistory[x, y] += 1
+			#	for x in range(priorHistory.shape[0]):
+			#		priorHistory[x,:] /= numpy.sum(priorHistory[x,:])
+			#		#prior[x, :] /= min(len(prev), self.priorSize)*1.0
+		    #
+			#	prior = recognizers.distance()
+			#	
+			#	print prior.shape
+			#	print priorHistory.shape
+			#	
+			#	for i in range(0, w.shape[0]):
+			#		w[i,:] += prior[i]
+			#	
+			#	#for i in range(0, w.shape[0]):
+			#	#	for j in range(0, w.shape[1]):
+			#	#		if j<prior.shape[1]:
+			#	#			w[i, j] += prior[i,j]*priorHistory[i,j]*self.alpha
 		
 			a = numpy.zeros((w.shape[0], 1)) + numpy.average(w)*self.beta
 			w = numpy.hstack((w,a))
 	
 			predicted = ai.predict(w)
 			prev.append([predicted, objectImages])
-			track.append(predicted)
-       
-			if ui != None:
+
+			
+			currentTrack = []
+			for (i,l) in enumerate(predicted):
+				tmp = [objects[i][0]+objects[i][2]/2.0, objects[i][1]+objects[i][3]/2.0, l]
+				currentTrack.append(tmp)
+
+			#track.append(predicted)
+			track.append(currentTrack)	
+			
+			if self.ui == True:
 				canvas = CVCanvas(frame)
        
 				if len(predicted)>0:
-					for i in range(0, len(objects)):					
+					for i in range(0, len(objects)):
 						label = predicted[i]
 						canvas.drawText('%d'%(label), (objects[i][0], objects[i][1]-3), (255, 255, 0))
 						canvas.drawRect(objects[i], (255,0,0))
 			
 				ui.update(canvas)
 
+		return track
+		
 if __name__ == '__main__':
-	v = VRoster()
+	v = VRoster(alpha=.1, beta=1, priorSize=5)
 	v.run()		
